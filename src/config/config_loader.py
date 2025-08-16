e25b1c83e77663366ceda5a91f737ee1dac1bf25
Jayant Singhal <jayantsinghal992@gmail.com>
2025-08-16 23:02:36 +0530

    Improved the response

--- src/config/config_loader.py
+++ src/config/config_loader.py
@@ -1,6 +1,9 @@
 import yaml
+from pathlib import Path
 
-def load_config(path: str = '../config.yaml'):
+BASE_DIR = Path(__file__).parent
+
+def load_config(path: str = f'{BASE_DIR}/config.yaml'):
     with open(path, 'r') as f:
         config = yaml.safe_load(f)
     return config
